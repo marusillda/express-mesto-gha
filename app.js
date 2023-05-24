8faf99b7a83f9f700ef16c7b92038f492043995e
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-05-25 02:09:08 +0300

    fix: исправить автотест

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const errorHandler = require('./middlewares/errorHandler');
-const auth = require('./middlewares/auth');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -11,7 +10,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
 
-app.use(auth);
+app.use((req, res, next) => {
+  req.user = {
+    _id: '646df21c1b61f7bc647ec642', // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
+
+  next();
+});
 
 app.use(userRouter);
 app.use(cardRouter);
