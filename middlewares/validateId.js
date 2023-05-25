e49e80c9080597b536eb35a0e62669d3ec65ba80
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-05-25 03:11:57 +0300

    fix: исправить опечатку правка автотестов

--- middlewares/validateId.js
+++ middlewares/validateId.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const createError = require('http-errors');
 
 const validateId = (req, res, next) => {
-  const id = Object.values(req.param)[0];
+  const id = Object.values(req.params)[0];
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
     next(createError(400, 'Параметр не является идентификатором'));
