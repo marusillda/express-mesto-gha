b702006aaa9f1fc68c1cccffeb7cacb1ae86dd76
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-06-08 19:01:13 +0300

    fix: исправить атотесты

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,8 @@ const {
 const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 
+const { TOKEN_SECRET = '421b5fa29e2f3344c4' } = process.env;
+
 const auth = (req, res, next) => {
   if (!req.headers.authorization) {
     return next(createError(HTTP_STATUS_UNAUTHORIZED, 'Пользователь не авторизован'));
@@ -12,7 +14,7 @@ const auth = (req, res, next) => {
   const token = req.headers.authorization.replace('Bearer ', '');
 
   try {
-    req.user = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = jwt.verify(token, TOKEN_SECRET);
     return next();
   } catch (err) {
     return next(createError(HTTP_STATUS_UNAUTHORIZED, 'Пользователь не авторизован'));
